a16424d8d57a748362858a335ae73f5f1f77c1df
7Limes <milesb2288@gmail.com>
2025-03-14 18:42:12 -0400

    Move get_ctx_pos

--- mlg1/compiler/util.py
+++ mlg1/compiler/util.py
@@ -13,15 +13,14 @@ def is_integer(s: str) -> bool:
     return bool(INTEGER_REGEX.match(s))
 
 
-def get_ctx_pos(ctx: ParserRuleContext) -> tuple[int, int]:
-    return ctx.start.line, ctx.start.column
-
-
 def error(position: tuple[int, int], line_text: str, message: str):
     print(f'{COLOR_ERROR}ERROR:', message)
     print(f'{position[0]+1} | {line_text}')
     print(f'{" " * (len(str(position[0]+1))+3+position[1])}^{COLOR_RESET}')
     sys.exit(1)
+
+def get_ctx_pos(ctx: ParserRuleContext) -> tuple[int, int]:
+    return ctx.start.line, ctx.start.column
 
 def error_ctx(ctx: ParserRuleContext, source_lines: list[str], message: str):
     line, column = get_ctx_pos(ctx)
